28f949c4987f24f469bf642f42e91e85f9e7b2ff
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 08:54:26 +0800

    feat: implement interface usecase

---
+++ app/src/main/java/com/example/dicodingevents/core/domain/usecase/EventInteractor.kt
@@ -0,0 +1,15 @@
+package com.example.dicodingevents.core.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.example.dicodingevents.core.data.Resource
+import com.example.dicodingevents.core.domain.model.Event
+import com.example.dicodingevents.core.domain.repository.IEventRepository
+
+class EventInteractor(private val eventRepository: IEventRepository): EventUseCase {
+    override fun getAllEvent() = eventRepository.getAllEvent()
+
+    override fun getFavoriteEvent() = eventRepository.getFavoriteEvent()
+
+    override fun setFavoriteEvent(event: Event, state: Boolean) = eventRepository.setFavoriteEvent(event, state)
+
+}
