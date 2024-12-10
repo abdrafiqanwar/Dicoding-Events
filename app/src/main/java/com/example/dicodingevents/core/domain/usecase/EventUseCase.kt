614c6f22634fe429539f7d0b609234fc50bc3623
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 19:42:53 +0800

    feat: flow data type in usecase

--- app/src/main/java/com/example/dicodingevents/core/domain/usecase/EventUseCase.kt
+++ app/src/main/java/com/example/dicodingevents/core/domain/usecase/EventUseCase.kt
@@ -1,11 +1,11 @@
 package com.example.dicodingevents.core.domain.usecase
 
-import androidx.lifecycle.LiveData
 import com.example.dicodingevents.core.data.Resource
 import com.example.dicodingevents.core.domain.model.Event
+import kotlinx.coroutines.flow.Flow
 
 interface EventUseCase {
-    fun getAllEvent(): LiveData<Resource<List<Event>>>
-    fun getFavoriteEvent(): LiveData<List<Event>>
+    fun getAllEvent(): Flow<Resource<List<Event>>>
+    fun getFavoriteEvent(): Flow<List<Event>>
     fun setFavoriteEvent(event: Event, state: Boolean)
 }
