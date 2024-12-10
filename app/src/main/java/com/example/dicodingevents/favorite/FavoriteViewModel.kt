2c06870cd2506e82d5262f0fac41e9eb7b2e0d54
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 19:43:17 +0800

    feat: convert flow as livedata

--- app/src/main/java/com/example/dicodingevents/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/dicodingevents/favorite/FavoriteViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.dicodingevents.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.dicodingevents.core.domain.usecase.EventUseCase
 
 class FavoriteViewModel(eventUseCase: EventUseCase) : ViewModel() {
-
-    val favoriteEvent = eventUseCase.getFavoriteEvent()
+    val favoriteEvent = eventUseCase.getFavoriteEvent().asLiveData()
 }
--- app/src/main/java/com/example/dicodingevents/home/MainViewModel.kt
+++ app/src/main/java/com/example/dicodingevents/home/MainViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.dicodingevents.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.dicodingevents.core.domain.usecase.EventUseCase
 
 class MainViewModel(eventUseCase: EventUseCase) : ViewModel() {
-
-    val event = eventUseCase.getAllEvent()
+    val event = eventUseCase.getAllEvent().asLiveData()
 }
