0a2a38b6c0763320d978b4b891f5a21575c624f0
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 17:32:12 +0800

    feat: load data from api

--- app/src/main/java/com/example/dicodingevents/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/dicodingevents/core/data/source/remote/RemoteDataSource.kt
@@ -1,44 +1,46 @@
 package com.example.dicodingevents.core.data.source.remote
 
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.dicodingevents.core.data.source.remote.network.ApiResponse
+import com.example.dicodingevents.core.data.source.remote.network.ApiService
 import com.example.dicodingevents.core.data.source.remote.response.EventResponse
-import com.example.dicodingevents.core.utils.JsonHelper
-import org.json.JSONException
+import com.example.dicodingevents.core.data.source.remote.response.ListEventResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
+class RemoteDataSource private constructor(private val apiService: ApiService) {
     companion object {
         @Volatile
         private var instance: RemoteDataSource? = null
 
-        fun getInstance(helper: JsonHelper): RemoteDataSource =
+        fun getInstance(service: ApiService): RemoteDataSource =
             instance ?: synchronized(this) {
-                instance ?: RemoteDataSource(helper)
+                instance ?: RemoteDataSource(service)
             }
     }
 
     fun getAllEvent(): LiveData<ApiResponse<List<EventResponse>>> {
         val resultData = MutableLiveData<ApiResponse<List<EventResponse>>>()
 
-        //get data from local json
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            try {
-                val dataArray = jsonHelper.loadData()
-                if (dataArray.isNotEmpty()) {
-                    resultData.value = ApiResponse.Success(dataArray)
-                } else {
-                    resultData.value = ApiResponse.Empty
-                }
-            } catch (e: JSONException){
-                resultData.value = ApiResponse.Error(e.toString())
-                Log.e("RemoteDataSource", e.toString())
+        //get data from remote api
+        val client = apiService.getList()
+
+        client.enqueue(object : Callback<ListEventResponse> {
+            override fun onResponse(
+                call: Call<ListEventResponse>, response: Response<ListEventResponse>
+            ) {
+                val dataArray = response.body()?.listEvents
+                resultData.value = if (dataArray != null) ApiResponse.Success(dataArray) else ApiResponse.Empty
             }
-        }, 2000)
+
+            override fun onFailure(call: Call<ListEventResponse>, t: Throwable) {
+                resultData.value = ApiResponse.Error(t.message.toString())
+                Log.e("RemoteDataSource", t.message.toString())
+            }
+        })
 
         return resultData
     }
--- app/src/main/java/com/example/dicodingevents/core/di/Injection.kt
+++ app/src/main/java/com/example/dicodingevents/core/di/Injection.kt
@@ -1,12 +1,11 @@
 package com.example.dicodingevents.core.di
 
 import android.content.Context
-
 import com.example.dicodingevents.core.data.source.local.LocalDataSource
 import com.example.dicodingevents.core.data.source.local.room.EventDatabase
-
 import com.example.dicodingevents.core.data.EventRepository
 import com.example.dicodingevents.core.data.source.remote.RemoteDataSource
+import com.example.dicodingevents.core.data.source.remote.network.ApiConfig
 import com.example.dicodingevents.core.domain.repository.IEventRepository
 import com.example.dicodingevents.core.domain.usecase.EventInteractor
 import com.example.dicodingevents.core.domain.usecase.EventUseCase
@@ -17,7 +16,7 @@ object Injection {
     private fun provideRepository(context: Context): IEventRepository {
         val database = EventDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.eventDao())
         val appExecutors = AppExecutors()
 
