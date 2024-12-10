34d71b135d576ed45d72fc603173b6f2d9642178
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 08:28:26 +0800

    feat: mapper entity <-> domain

--- app/src/main/java/com/example/dicodingevents/core/utils/DataMapper.kt
+++ app/src/main/java/com/example/dicodingevents/core/utils/DataMapper.kt
@@ -2,6 +2,7 @@ package com.example.dicodingevents.core.utils
 
 import com.example.dicodingevents.core.data.source.local.entity.EventEntity
 import com.example.dicodingevents.core.data.source.remote.response.EventResponse
+import com.example.dicodingevents.core.domain.model.Event
 
 object DataMapper {
     fun mapResponsesToEntities(input: List<EventResponse>): List<EventEntity> {
@@ -27,4 +28,43 @@ object DataMapper {
         }
         return eventList
     }
+
+    fun mapEntitiesToDomain(input: List<EventEntity>): List<Event> =
+        input.map {
+            Event(
+                id = it.id,
+                summary = it.summary,
+                mediaCover = it.mediaCover,
+                registrants = it.registrants,
+                imageLogo = it.imageLogo,
+                link = it.link,
+                description = it.description,
+                ownerName = it.ownerName,
+                cityName = it.cityName,
+                quota = it.quota,
+                name = it.name,
+                beginTime = it.beginTime,
+                endTime = it.endTime,
+                category = it.category,
+                isFavorite = it.isFavorite
+            )
+        }
+
+    fun mapDomainToEntity(input: Event) = EventEntity(
+        id = input.id,
+        summary = input.summary,
+        mediaCover = input.mediaCover,
+        registrants = input.registrants,
+        imageLogo = input.imageLogo,
+        link = input.link,
+        description = input.description,
+        ownerName = input.ownerName,
+        cityName = input.cityName,
+        quota = input.quota,
+        name = input.name,
+        beginTime = input.beginTime,
+        endTime = input.endTime,
+        category = input.category,
+        isFavorite = input.isFavorite
+    )
 }
