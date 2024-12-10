260d03fc91739ddf5b1ac1d7a35c34833fa7662c
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-10 19:40:00 +0800

    feat: local with flow

--- app/src/main/java/com/example/dicodingevents/core/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/example/dicodingevents/core/data/source/local/LocalDataSource.kt
@@ -3,6 +3,7 @@ package com.example.dicodingevents.core.data.source.local
 import androidx.lifecycle.LiveData
 import com.example.dicodingevents.core.data.source.local.entity.EventEntity
 import com.example.dicodingevents.core.data.source.local.room.EventDao
+import kotlinx.coroutines.flow.Flow
 
 class LocalDataSource private constructor(private val eventDao: EventDao) {
 
@@ -15,11 +16,11 @@ class LocalDataSource private constructor(private val eventDao: EventDao) {
             }
     }
 
-    fun getAllEvent(): LiveData<List<EventEntity>> = eventDao.getAllEvent()
+    fun getAllEvent(): Flow<List<EventEntity>> = eventDao.getAllEvent()
 
-    fun getFavoriteEvent(): LiveData<List<EventEntity>> = eventDao.getFavoriteEvent()
+    fun getFavoriteEvent(): Flow<List<EventEntity>> = eventDao.getFavoriteEvent()
 
-    fun insertEvent(eventList: List<EventEntity>) = eventDao.insertEvent(eventList)
+    suspend fun insertEvent(eventList: List<EventEntity>) = eventDao.insertEvent(eventList)
 
     fun setFavoriteEvent(event: EventEntity, newState: Boolean) {
         event.isFavorite = newState
--- app/src/main/java/com/example/dicodingevents/core/data/source/local/room/EventDao.kt
+++ app/src/main/java/com/example/dicodingevents/core/data/source/local/room/EventDao.kt
@@ -1,20 +1,20 @@
 package com.example.dicodingevents.core.data.source.local.room
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.dicodingevents.core.data.source.local.entity.EventEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EventDao {
 
     @Query("SELECT * FROM event")
-    fun getAllEvent(): LiveData<List<EventEntity>>
+    fun getAllEvent(): Flow<List<EventEntity>>
 
     @Query("SELECT * FROM event where isFavorite = 1")
-    fun getFavoriteEvent(): LiveData<List<EventEntity>>
+    fun getFavoriteEvent(): Flow<List<EventEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertEvent(event: List<EventEntity>)
+    suspend fun insertEvent(event: List<EventEntity>)
 
     @Update
     fun updateFavoriteEvent(event: EventEntity)
