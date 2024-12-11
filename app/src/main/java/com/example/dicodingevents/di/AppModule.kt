1464ce27b0432fb48b11fb0a352702bf185f5496
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-12-11 13:11:10 +0800

    feat: app module

---
+++ app/src/main/java/com/example/dicodingevents/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.dicodingevents.di
+
+import com.example.dicodingevents.core.domain.usecase.EventInteractor
+import com.example.dicodingevents.core.domain.usecase.EventUseCase
+import com.example.dicodingevents.detail.DetailViewModel
+import com.example.dicodingevents.favorite.FavoriteViewModel
+import com.example.dicodingevents.home.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<EventUseCase> { EventInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
+}
